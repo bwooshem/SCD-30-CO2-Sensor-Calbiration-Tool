ac3fdc3b36df2f6cf97c3096d0021dd3425d16b6
Brian Woo-Shem <85897571+bwooshem@users.noreply.github.com>
2022-10-10 22:50:53 -0700

    Cleanup & better commenting

--- SCD-30_CO2_Calibration_Tool.ino
+++ SCD-30_CO2_Calibration_Tool.ino
@@ -1,7 +1,20 @@
-// DIY CO2 Sensor SCD-30 Forced Recalibration Code
-// CC By-NC 2022 Eric & Brian Woo-Shem
-// Part of DIY Frugal Arduino CO2 Sensor
-// https://github.com/ericwooshem/DIY-Frugal-Arduino-CO2-Sensor
+/*  DIY CO2 Sensor SCD-30 Forced Recalibration Code
+ *  
+ *  (C) 2022 Brian & Eric Woo-Shem
+ *  Version 1.1, updated 2022-10-10
+ *  
+ *  https://github.com/bwooshem/SCD-30-CO2-Sensor-Calbiration-Tool
+ *  
+ *  Part of DIY Frugal Arduino CO2 Sensor: https://github.com/ericwooshem/DIY-Frugal-Arduino-CO2-Sensor
+ *  
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the 
+ *  GNU General Public License version 3 as published by the Free Software Foundation.
+ *  
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ *  GNU General Public License at https://www.gnu.org/licenses/gpl-3.0.en.html for more details.
+  */
+
 /*
 Info:
 Code to quickly recalibrate a SCD-30 sensor using Arduino. If sensor is not returning values around 400-450ppm when outside, use this code to reset it.
@@ -10,8 +23,9 @@ Also disables automatic self-calibration because it has caused problems.
 Afterward it will take measurements and display them to screen indefinitely so you can check your work. 
 Does NOT log data. Does NOT use RTC.
 
-LCD = Liquid Crystal Display (Screen)
+Loop takes up to 2 seconds, so may have to hold down buttons for a moment before it runs.
 
+LCD = Liquid Crystal Display (Screen)
 
 Resources Used:
 https://learn.adafruit.com/adafruit-scd30
@@ -22,11 +36,13 @@ https://learn.adafruit.com/adafruit-scd30
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2); // Set LCD Screen pins
 Adafruit_SCD30  scd30; // Define Sensor
 
+//Set pins
 const int UP = A1;
 const int DOWN = A2;
 const int RECAL = A0;
 
-int ppmValue = 420;
+//Target PPM setting for sensor. Should be equal to the expected value in calibration environment.
+int ppmValue = 420; //ppm
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void setup() { // Start setup
@@ -54,27 +70,7 @@ void setup() { // Start setup
   lcd.setCursor(0,0);
   lcd.print("(C)Eric Woo-Shem");
   delay(1000);
-  //lcd.clear();
   
-// Force sensor to grab a few measurements to deal with first value = zero and sensor startup time bug.
-/*
-  int i = 1;
-  while(i<4){
-    if (scd30.dataReady()){ // If CO2 data
-        if (!scd30.read()) //If probelm reading data
-          {
-            return; // Don't do the the following ↓↓↓
-           }
-        lcd.clear();
-        lcd.setCursor(0,0); // Set LCD Screen cursor position
-        lcd.print("Initializing..."); // Write to LCD Screen 
-        lcd.setCursor(0,1); // Set LCD Screen cursor position
-        lcd.print("Co2: "+String(scd30.CO2)); // Write the CO2 level to the LCD Screen
-    }
-    i += 1;
-  }
-  delay(1000);
-*/
 
 } // End setup
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -92,24 +88,23 @@ void loop() {
     lcd.setCursor(0,1); // Set LCD Screen cursor position
     lcd.print("CO2: " + String(scd30.CO2)); // Write the CO2 level to the LCD Screen
 
-    //int upVal=analogRead(UP);
-    //int downVal=analogRead(DOWN);
-    //int recVal=analogRead(RECAL);
     int upVal=digitalRead(UP);
     int downVal=digitalRead(DOWN);
     int recVal=digitalRead(RECAL);
 
+    //Serial lines for debugging. OK to remove
     Serial.println("UP: " + String(upVal));
     Serial.println("DOWN: " + String(downVal));
     Serial.println("RECAL: " + String(recVal));
     
-    if (upVal==HIGH){
-        ppmValue += 10;
+    //Detect each button
+    if (upVal==HIGH){ //Push button for "UP" is activated
+        ppmValue += 10; //Increase CO2 target PPM by 10
     }
-    else if (downVal==HIGH){
-        ppmValue -= 10;
+    else if (downVal==HIGH){ //Push button for "DOWN" is activated
+        ppmValue -= 10; //Decrease CO2 target PPM by 10
     }
-    else if (recVal==HIGH){
+    else if (recVal==HIGH){ //Push button to SET NEW CALIBRATION LEVELS is activated
         
         // Disable automatic self-calibration because it tends to make sensors get off over time
         // Note: if set to true, self-calibration is enabled.
@@ -140,11 +135,11 @@ void loop() {
                 Serial.println("SCD30 not ready :(");
                 lcd.setCursor(0,0); // Set LCD Screen cursor position
                 lcd.print("Calibrated!"); // Write to LCD Screen 
-                delay(500);
+                delay(500); //Prevent immediate retry
             }
             Serial.println("i = " + String(i));
             i += 1;
-        }
-    }
-  }
-}
+        } //end while
+    } //end else if RECALIBRATE button is activated
+  } //End if dataready
+} //End Loop()
